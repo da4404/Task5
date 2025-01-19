116ceeee1ce0466ab2779e5a1b5450b01f81228e
do4404 <da4404@bs.amalnet.k12.il>
2025-01-19 15:14:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/task5/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.task5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=(TextView)findViewById(R.id.textView);
+    }
+    public void changeText(View view) {
+        text.setText(" Oh, yea, I’ve been clicked!");
+    }
+}
